aaa83b2acdae912e2f2f2311b294b89badd1b54e
changmin1996 <changmin@wego-robotics.com>
2025-04-17 14:58:08 +0900

    code finish

--- neupan_ros/neupan_node.py
+++ neupan_ros/neupan_node.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from geometry_msgs.msg import Twist, PoseStamped, TransformStamped
+from geometry_msgs.msg import Twist, PoseStamped, TransformStamped, Quaternion
 from nav_msgs.msg import Path
 from visualization_msgs.msg import MarkerArray, Marker
 from sensor_msgs.msg import LaserScan
@@ -48,7 +48,7 @@ class NeupanNode(Node):
         self.flip_angle_ = self.get_parameter('flip_angle').value
 
         pan = {'dune_checkpoint': self.dune_checkpoint_}
-        self.neupan_planner = neupan.init_from_yaml(
+        self.neupan_planner_ = neupan.init_from_yaml(
             self.planner_config_file_, pan=pan
         )
 
@@ -86,13 +86,57 @@ class NeupanNode(Node):
                                                        'neupan_goal',
                                                        self.goalCallback,
                                                        10)
-        
-
 
         self.timer_ = self.create_timer(0.02, self.run)
     
     def run(self):
-        pass
+        try:
+            now = rclpy.time.Time()
+            trans = self.tf_buffer_.lookup_transform(self.map_frame_, self.base_frame_, now)
+            t = trans.transform.translation
+            r = trans.transform.rotation
+
+            x, y = t.x, t.y
+            rot = [r.x, r.y, r.z, r.w]
+            yaw = self.quat_to_yaw_list(rot)
+
+            self.robot_state_ = np.array([x, y, yaw]).reshape(3, 1)
+
+        except TransformException as ex:
+            self.get_logger().warn(f"TF transform failed: {ex}")
+            return
+
+        if self.robot_state_ is None:
+            return
+
+        if len(self.neupan_planner_.waypoints) >= 1 and self.neupan_planner_.initial_path is None:
+            self.neupan_planner_.set_initial_path_from_state(self.robot_state_)
+
+        if self.neupan_planner_.initial_path is None:
+            self.get_logger().warn("Waiting for initial path...")
+            return
+
+        self.ref_state_pub_.publish(self.generate_path_msg(self.neupan_planner_.initial_path))
+
+        if self.obstacle_points_ is None:
+            self.get_logger().warn("No obstacle points, path tracking only")
+
+        action, info = self.neupan_planner_(self.robot_state_, self.obstacle_points_)
+
+        self.stop_ = info["stop"]
+        self.arrive_ = info["arrive"]
+
+        if self.arrive_:
+            self.get_logger().info("Arrived at target.")
+
+        self.plan_pub_.publish(self.generate_path_msg(info["opt_state_list"]))
+        self.ref_state_pub_.publish(self.generate_path_msg(info["ref_state_list"]))
+        self.vel_pub_.publish(self.generate_twist_msg(action))
+
+        self.point_markers_pub_dune_.publish(self.generate_dune_points_markers_msg())
+        self.point_marker_pub__nrmp_.publish(self.generate_nrmp_points_markers_msg())
+        self.robot_marker_pub_.publish(self.generate_robot_marker_msg())
+
 
     def laserCallback(self, msg):
         if self.robot_state_ is None:
@@ -139,19 +183,157 @@ class NeupanNode(Node):
 
             yaw = self.quat_to_yaw_list(rotation)
             trans_matrix, rot_matrix = get_transform(np.c_[x, y, yaw].reshape(3, 1))
-            self.obstacle_points = rot_matrix @ point_array + trans_matrix
-            return self.obstacle_points
+            self.obstacle_points_ = rot_matrix @ point_array + trans_matrix
+            return self.obstacle_points_
             
         except TransformException as ex:
             self.get_logger().warn(f"Transform failed: {ex}")
             return
 
     def pathCallback(self, msg):
-        pass
+        self.get_logger().info("target path update")
+
+        initial_point_list=[]
+
+        for p in msg.poses:
+            x = p.pose.position.x
+            y = p.pose.position.y
+            theta = self.quat_to_yaw(p.pose.orientation)
+
+            points = np.array([x, y, theta, 1]).reshape(4, 1)
+            initial_point_list.append(points)
+        
+        if self.neupan_planner_.initial_path is None or self.refresh_initial_path_:
+            self.neupan_planner_.set_initial_path(initial_point_list)
+
 
     def goalCallback(self, msg):
-        pass
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        theta = self.quat_to_yaw(msg.pose.orientation)
+
+        self.goal_ = np.array([[x], [y], [theta]])
+
+        self.get_logger().info(f"Set goal: {[x, y, theta]}")
+        self.neupan_planner_.update_initial_path_from_goal(self.robot_state_, self.goal_)
     
+    def generate_path_msg(self, path_list):
+        path = Path()
+        path.header.frame_id = self.map_frame_
+        path.header.stamp = self.get_clock().now().to_msg()
+    
+        for index, point in enumerate(path_list):
+            ps = PoseStamped()
+            ps.header.frame_id = self.map_frame_
+            ps.header.stamp = self.get_clock().now().to_msg()
+            ps.pose.position.x = point[0, 0]
+            ps.pose.position.y = point[1, 0]
+            ps.pose.orientation.w = 1.0
+            path.poses.append(ps)
+    
+        return path
+
+    def generate_twist_msg(self, vel):
+        msg = Twist()
+
+        if vel is None or self.stop_ or self.arrive_:
+            return msg
+
+        msg.linear.x = vel[0, 0]
+        msg.angular.z = vel[1, 0]
+        return msg
+
+    def generate_dune_points_markers_msg(self):
+        marker_array = MarkerArray()
+
+        if self.neupan_planner_.dune_points is None:
+            return marker_array
+
+        for index, point in enumerate(self.neupan_planner_.dune_points.T):
+            marker = Marker()
+            marker.header.frame_id = self.map_frame_
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.id = index
+            marker.type = Marker.SPHERE
+            marker.scale.x = self.marker_size_
+            marker.scale.y = self.marker_size_
+            marker.scale.z = self.marker_size_
+            marker.color.a = 1.0
+            marker.color.r = 160 / 255
+            marker.color.g = 32 / 255
+            marker.color.b = 240 / 255
+            marker.pose.position.x = point[0]
+            marker.pose.position.y = point[1]
+            marker.pose.position.z = 0.3
+            marker_array.markers.append(marker)
+
+        return marker_array
+
+    def generate_nrmp_points_markers_msg(self):
+        marker_array = MarkerArray()
+
+        if self.neupan_planner_.nrmp_points is None:
+            return marker_array
+
+        for index, point in enumerate(self.neupan_planner_.nrmp_points.T):
+            marker = Marker()
+            marker.header.frame_id = self.map_frame_
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.id = index
+            marker.type = Marker.SPHERE
+            marker.scale.x = self.marker_size_
+            marker.scale.y = self.marker_size_
+            marker.scale.z = self.marker_size_
+            marker.color.a = 1.0
+            marker.color.r = 255 / 255
+            marker.color.g = 128 / 255
+            marker.color.b = 0 / 255
+            marker.pose.position.x = point[0]
+            marker.pose.position.y = point[1]
+            marker.pose.position.z = 0.3
+            marker_array.markers.append(marker)
+
+        return marker_array
+
+    def generate_robot_marker_msg(self):
+        marker = Marker()
+        marker.header.frame_id = self.map_frame_
+        marker.header.stamp = self.get_clock().now().to_msg()
+        marker.id = 0
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+
+        if self.neupan_planner_.robot.shape == "rectangle":
+            length = self.neupan_planner_.robot.length
+            width = self.neupan_planner_.robot.width
+            wheelbase = self.neupan_planner_.robot.wheelbase
+
+            marker.scale.x = length
+            marker.scale.y = width
+            marker.scale.z = self.marker_z_
+            marker.type = Marker.CUBE
+
+            x = self.robot_state_[0, 0]
+            y = self.robot_state_[1, 0]
+            theta = self.robot_state_[2, 0]
+
+            if self.neupan_planner_.robot.kinematics == "acker":
+                diff_len = (length - wheelbase) / 2
+                marker_x = x + diff_len * cos(theta)
+                marker_y = y + diff_len * sin(theta)
+            else:
+                marker_x = x
+                marker_y = y
+
+            marker.pose.position.x = marker_x
+            marker.pose.position.y = marker_y
+            marker.pose.position.z = 0
+            marker.pose.orientation = self.yaw_to_quat(theta)
+
+        return marker
+
     def quat_to_yaw_list(self, quater):
         x = quater[0]
         y = quater[1]
@@ -161,6 +343,27 @@ class NeupanNode(Node):
         yaw = atan2(2 * (w * z + x * y), 1 - 2 * (pow(z, 2) + pow(y, 2)))
 
         return yaw
+
+    @staticmethod
+    def quat_to_yaw(quater):
+        x = quater.x
+        y = quater.y
+        z = quater.z
+        w = quater.w
+        raw = atan2(2 * (w * z + x * y), 1 - 2 * (pow(z, 2) + pow(y, 2)))
+        return raw
+    
+    @staticmethod
+    def yaw_to_quat(yaw):
+
+        quater = Quaternion()
+
+        quater.x = 0
+        quater.y = 0
+        quater.z = sin(yaw / 2)
+        quater.w = cos(yaw / 2)
+
+        return quater
 
 def main(args=None):
     rclpy.init(args=args)
